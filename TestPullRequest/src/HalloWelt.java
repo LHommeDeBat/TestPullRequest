8a4f50f58146ecda2fe204a78add37a2b0630582
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2018-10-18 11:38:24 +0200

    Konsolenoutput angepasst

--- TestPullRequest/src/HalloWelt.java
+++ TestPullRequest/src/HalloWelt.java
@@ -13,7 +13,7 @@ public class HalloWelt {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hallo Welt!");
+		System.out.println("Guten Morgen Welt!");
 	}
 
 }
