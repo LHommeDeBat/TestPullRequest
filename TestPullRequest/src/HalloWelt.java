5ad692a7c432e3e6b6cd197050623dd7d4169139
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2018-10-18 11:31:08 +0200

    Java-Projekt hinzugefügt

---
+++ TestPullRequest/src/HalloWelt.java
@@ -0,0 +1,19 @@
+/**
+ * Diese Klasse dient zum testen von GitHub's Pullrequests
+ * 
+ * @author Stefan Basaric
+ *
+ */
+public class HalloWelt {
+
+	/**
+	 * In dieser Methode wird das Java-Projekt gestartet und sie gibt "Hallo Welt"
+	 * in der Konsole aus.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt!");
+	}
+
+}
