9693d4eca7f0e0c3e6d68df8dc1235f6b95bc4a4
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2018-12-31 13:05:49 +0100

    Added new method to SubCalc

--- TestPullRequest/src/testPackage/SubCalc.java
+++ TestPullRequest/src/testPackage/SubCalc.java
@@ -2,6 +2,10 @@ package testPackage;
 
 public class SubCalc extends Calc {
 	
+	public Integer addition(int a, int b, int c) {
+		return a - b;
+	}
+	
 	public Integer subtract(int a, int b) {
 		return a - b;
 	}
