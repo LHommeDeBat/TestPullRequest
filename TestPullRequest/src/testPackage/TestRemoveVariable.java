0f1b0daa851f542a8aa364b5c3d3f0285b3ee31a
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-01-14 17:42:22 +0100

    Added new method

--- TestPullRequest/src/testPackage/TestRemoveVariable.java
+++ TestPullRequest/src/testPackage/TestRemoveVariable.java
@@ -4,13 +4,20 @@ public class TestRemoveVariable {
 
 	private int a;
 	private int b;
-	
+
 	public int getA() {
 		return a;
 	}
-	
+
 	public void setA(int a) {
 		this.a = a;
+	}
+
+	public void doStuff() {
+		int a = 3;
+		int b = 4;
+		int c = a + b;
+		this.b = b;
 	}
 
 }
